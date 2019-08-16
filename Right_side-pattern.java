88f8bb9debabadf518a67803f2878ce9da81e2ff
prabakaran1998 <42084128+prabakaran1998@users.noreply.github.com>
2019-08-16 15:59:31 +0530

    Update Right_side-pattern.java

--- Right_side-pattern.java
+++ Right_side-pattern.java
@@ -2,21 +2,18 @@ package Programs;
 
 import java.util.Scanner;
 
-public class Left_Side_pattern {
+public class Right_Pattern {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt();
-		for(int i=0;i<n;i++) {
-			for(int j=n-1;j>i;j--)
-			{
-				System.out.print(" ");
+			for(int i=1;i<=n;i++) {
+				for(int j=1;j<=i;j++) {
+				      
+					System.out.print("*");
 				}
-			for(int k=0;k<=i;k++) {
-				System.out.print("*");
-			}
-			System.out.println();
+				System.out.println();
 			}
 	}
 
